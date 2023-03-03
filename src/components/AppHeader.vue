750e2c246c9321a6cd0c10203aa2265ce12b1c43
Federica Pozzer <federica.pozzer@gmail.com>
2023-03-03 12:49:45 +0100

    init project

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script>
+
+</script>
+
+
+<template>
+    <div>
+        <h1 class="container mt-5">Hi Kik *.*</h1>
+    </div>
+
+</template>
+
+
+<style lang="scss" scoped>
+
+div {
+    h1 {
+        color: palevioletred;
+    }
+}
+
+
+</style>
