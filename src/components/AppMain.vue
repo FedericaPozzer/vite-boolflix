09632e47ebb73d27a970d9168ba9f9924353978a
Federica Pozzer <federica.pozzer@gmail.com>
2023-03-07 15:23:29 +0100

    Random card that flips - ok! Now apply it..

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -15,6 +15,22 @@ export default {
 <template>
     <div class="main-container">
 
+        <div class="container-fluid pippo">
+            <div class="cardu">
+                <div class="front">
+                    <h2>CARD'S FRONT</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, sed!</p>
+                </div>
+                <div class="back">
+                    <h2>CARD'S BACK</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, et.</p>
+                </div>
+            </div>
+        </div>
+
+
+
+
         <div class="container-fluid">
             <h3>MOVIES</h3>
             <div class="row row-cols-5">
@@ -50,5 +66,43 @@ export default {
         color: #ffffff;
     }
 }
+
+.pippo {
+    width: 350px;
+    height: 500px;
+    .cardu {
+        height: 100%;
+        width: 100%;
+        position: relative;
+        transition: transform 1500ms;
+        transform-style: preserve-3d;
+
+        .back, .front {
+            height: 100%;
+            width: 100%;
+            padding: 10px;
+            border-radius: 2rem;
+            box-shadow:  0  0 5px 2px rgba(50, 50, 50, 0.25);
+            position: absolute;
+            backface-visibility: hidden;
+        }
+
+        .front {
+            background-color: aquamarine;
+        }
+
+        .back {
+            background-color: pink;
+            transform: rotateY(180deg);
+        }
+
+
+    }
+}
+
+.pippo:hover .cardu {
+            cursor: pointer;
+            transform: rotateY(180deg);
+        }
 
 </style>
--- src/data/store.js
+++ src/data/store.js
@@ -1,6 +1,6 @@
 import { reactive } from "vue";
 
-
+// questo è un export e non un export default perchè l'export default non accetta dichiarazioni di variabili.. per questo motivo (perchè ho una variabile) quando poi la richiamo altrove lo faccio tra parentesi graffe { store }. Se la costante si chiamasse pippo dovrei richiamarla con { pippo }.
 export const store = reactive({
 
     foundTitles : [], //all titles
